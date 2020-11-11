681ced6ccfd1009a73ed03e064fa1d6964f03d47
Alex Withington-Smith <alex@awithsmith.com>
2020-11-11 16:13:59 +0000

    added #attack

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,12 @@
 class Player
   attr_reader :name
+  attr_accessor :health
   def initialize(name)
     @name = name
+    @health = 69
+  end
+
+  def attack(player)
+    player.health -= 10
   end
 end
